2cffb7b89cccc3b6e939cacbd5d01cea76cf7c0e
Joost Verweijen <joosiiiv@gmail.com>
2022-11-02 12:40:22 +0100

    Basis website
    De basis van de website gemaakt. De eerste klikbare fakkel is toegevoegd.

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,23 @@
-// JavaScript Document
-console.log("Howdy!");
+function Flashlight(e) {
+    let x = e.clientX || e.touches[0].clientX
+    let y = e.clientY || e.touches[0].clientY
+
+    document.documentElement.style.setProperty('--cursorX', x + 'px')
+    document.documentElement.style.setProperty('--cursorY', y + 'px')
+}
+
+document.addEventListener('mousemove', Flashlight)
+document.addEventListener('touchmove', Flashlight)
+
+// Bron gebruikt: https://codemyui.com/flashlight-mouse-pointer/
+
+
+
+const torch1 = document.querySelector('.gen1 img')
+torch1.addEventListener('click', function () {
+    document.querySelector('.gen1 img').src = "images/torch-px.gif"
+
+    document.querySelector('.gen1 section').style.display = 'block'
+    document.querySelector('.gen1 div').style.display = 'block'
+    document.querySelector('.gen2 img').style.display = 'block'
+})

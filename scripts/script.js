349296450d13de7ecc0ea27e9eb23e5965593f1a
Joost Verweijen <joosiiiv@gmail.com>
2022-11-07 09:08:57 +0100

    Flashlight & Torches

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
+const Gen1 = document.querySelector('article:first-of-type')
+const Gen2 = document.querySelector('article:nth-of-type(2)')
+const Gen3 = document.querySelector('article:nth-of-type(3)')
+const Gen4 = document.querySelector('article:nth-of-type(4)')
+const Gen5 = document.querySelector('article:nth-of-type(5)')
+
+
 function Flashlight(e) {
     let x = e.clientX || e.touches[0].clientX
     let y = e.clientY || e.touches[0].clientY
@@ -12,12 +19,66 @@ document.addEventListener('touchmove', Flashlight)
 // Bron gebruikt: https://codemyui.com/flashlight-mouse-pointer/
 
 
-
-const torch1 = document.querySelector('.gen1 img')
+const torch1 = document.querySelector('a:first-of-type img')
 torch1.addEventListener('click', function () {
-    document.querySelector('.gen1 img').src = "images/torch-px.gif"
+    torch1.src = "images/torch-px.gif"
 
-    document.querySelector('.gen1 section').style.display = 'block'
-    document.querySelector('.gen1 div').style.display = 'block'
-    document.querySelector('.gen2 img').style.display = 'block'
+    Gen1.style.display = 'block'
+    document.querySelector('a:nth-of-type(2) img').style.display = 'block'
+
+    Gen2.style.display = 'none'
+    Gen3.style.display = 'none'
+    Gen4.style.display = 'none'
+    Gen5.style.display = 'none'
+})
+
+const torch2 = document.querySelector('a:nth-of-type(2) img')
+torch2.addEventListener('click', function () {
+    torch2.src = "images/torch-px.gif"
+
+    Gen2.style.display = 'block'
+    document.querySelector('a:nth-of-type(3) img').style.display = 'block'
+
+    Gen1.style.display = 'none'
+    Gen3.style.display = 'none'
+    Gen4.style.display = 'none'
+    Gen5.style.display = 'none'
+})
+
+const torch3 = document.querySelector('a:nth-of-type(3) img')
+torch3.addEventListener('click', function () {
+    torch3.src = "images/torch-px.gif"
+
+    Gen3.style.display = 'block'
+    document.querySelector('a:nth-of-type(4) img').style.display = 'block'
+
+    Gen1.style.display = 'none'
+    Gen2.style.display = 'none'
+    Gen4.style.display = 'none'
+    Gen5.style.display = 'none'
+})
+
+const torch4 = document.querySelector('a:nth-of-type(4) img')
+torch4.addEventListener('click', function () {
+    torch4.src = "images/torch-px.gif"
+
+    Gen4.style.display = 'block'
+    document.querySelector('a:nth-of-type(5) img').style.display = 'block'
+
+    Gen1.style.display = 'none'
+    Gen2.style.display = 'none'
+    Gen3.style.display = 'none'
+    Gen5.style.display = 'none'
+})
+
+const torch5 = document.querySelector('a:nth-of-type(5) img')
+torch5.addEventListener('click', function () {
+    torch5.src = "images/torch-px.gif"
+
+    Gen5.style.display = 'block'
+
+    Gen1.style.display = 'none'
+    Gen2.style.display = 'none'
+    Gen3.style.display = 'none'
+    Gen4.style.display = 'none'
 })
